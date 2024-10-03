1a93240f3ef7a9405b6849e27151feeb922a3cc2
Nguyễn Văn Dương <vanduongk1309@gmail.com>
2024-10-03 22:32:43 +0700

    Code mau migration, query data, template

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class DoiBong(models.Model):
+    ma_doi_bong = models.AutoField(primary_key=True)
+    ten = models.CharField(max_length=100)
+    san_van_dong = models.CharField(max_length=100)
+    nam_thanh_lap = models.IntegerField()
+
+    class Meta:
+        db_table = 'DoiBong'
+
+
+class ThanhTichDoiBong(models.Model):
+    ma_thanh_tich = models.AutoField(primary_key=True)
+    doi_bong_id = models.ForeignKey(DoiBong, on_delete=models.CASCADE)
+    ten_thanh_tich = models.CharField(max_length=255)
+    nam_thanh_tich = models.IntegerField()
+    mo_ta = models.TextField()
+
+    class Meta:
+        db_table = 'ThanhTichDoiBong'
