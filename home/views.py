8d8492df66eae877fd912ea2e392d3d28a0e52c4
Nguyễn Văn Dương <vanduongk1309@gmail.com>
2024-10-05 23:37:07 +0700

    Thêm trang hop dong

--- home/views.py
+++ home/views.py
@@ -4,37 +4,41 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 
-from home.models import DoiBong
+from home.models import DoiBong, HopDong
 
-# @login_required(login_url="/login/")
+@login_required(login_url="/login/")
 def index(request):
     context = {'segment': 'index'}
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
 
-# @login_required(login_url="/login/")
+@login_required(login_url="/login/")
 def cau_lac_bo(request):
     context = {'segment': 'cau_lac_bo'}
     html_template = loader.get_template('home/cau-lac-bo.html')
     return HttpResponse(html_template.render(context, request))
 
 
-# @login_required(login_url="/login/")
+@login_required(login_url="/login/")
 def cau_thu(request):
     context = {'segment': 'cau_thu'}
     html_template = loader.get_template('home/cau-thu.html')
     return HttpResponse(html_template.render(context, request))
 
 
-# @login_required(login_url="/login/")
+@login_required(login_url="/login/")
 def huan_luyen_vien(request):
     context = {'segment': 'huan_luyen_vien'}
     html_template = loader.get_template('home/huan-luyen-vien.html')
     return HttpResponse(html_template.render(context, request))
 
 
-# @login_required(login_url="/login/")
+@login_required(login_url="/login/")
 def hop_dong(request):
-    context = {'segment': 'hop_dong'}
+    ds_hop_dong = HopDong.objects.all().order_by('ngay_bat_dau')
+    context = {
+        'segment': 'hop_dong',
+        'ds_hop_dong': ds_hop_dong
+    }
     html_template = loader.get_template('home/hop-dong.html')
     return HttpResponse(html_template.render(context, request))
