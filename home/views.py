73d01e21e981c0531b7bb29c3e88552aeed37ae9
Nguyễn Văn Dương <vanduongk1309@gmail.com>
2024-10-06 13:37:12 +0700

    Thêm thống kê, biểu đồ trang index

--- home/views.py
+++ home/views.py
@@ -1,14 +1,36 @@
+import datetime
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 
-from home.models import DoiBong, HopDong
+from home.models import DoiBong, HopDong, CauThu, HuanLuyenVien
 
 @login_required(login_url="/login/")
 def index(request):
     context = {'segment': 'index'}
+    s_cau_thu = CauThu.objects.count()
+    s_hlv = HuanLuyenVien.objects.count()
+    s_hop_dong = HopDong.objects.count()
+    s_hop_dong_thang_truoc = HopDong.objects.filter(
+        ngay_bat_dau__lt=datetime.date.today() - datetime.timedelta(days=7)
+    ).count()
+    tong_quan = {
+        "cau_thu": s_cau_thu,
+        "hlv": s_hlv,
+        "hop_dong": {
+            "so_luong": s_hop_dong,
+            "tang": round((s_hop_dong - s_hop_dong_thang_truoc) / s_hop_dong_thang_truoc * 100, 2)
+        }
+    }
+    bieu_do_luong = {
+        "labels": ["Cầu thủ", "Huấn luyện viên"],
+        "data": [s_cau_thu, s_hlv],
+    }
+    context['tong_quan'] = tong_quan
+    context['bieu_do_luong'] = bieu_do_luong
+
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
 
